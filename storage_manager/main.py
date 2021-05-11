6dfa90e995827bdbf5df1206f3b9025dc11f67c2
Franco Liberali <franco.liberali@gmail.com>
2021-05-11 17:56:46 -0300

    meter lock para escribir en el indice por dia

--- storage_manager/main.py
+++ storage_manager/main.py
@@ -1,6 +1,6 @@
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
-from threading import Thread
+from threading import Lock, Thread
 from datetime import datetime
 import json
 
@@ -11,6 +11,8 @@ from common.common import STORAGE_MANAGER_WRITE_PORT, STORAGE_MANAGER_READ_PORT
 from common.responses import respond_not_found, respond_ok
 from common.safe_tcp_socket import SafeTCPSocket
 from common.block_interface import send_hash_and_block_json, recv_hash_and_block_json, recv_hash
+
+day_indexs_locks = {}
 
 def write_block(block_hash, block_json):
     # TODO responder un ok?
@@ -24,20 +26,25 @@ def write_block(block_hash, block_json):
     day_string = minute.replace(hour=0, minute=0).strftime('%Y-%m-%d')
     day_index_file_path = f"./blockchain_files/minutes_index/{day_string}.json"
 
-    # TODO este necesita file locks porque se escribe y lee el mismo archivo
-    try:
-        with open(day_index_file_path, "r") as index_file:
-            day_index = json.load(index_file)
-    except FileNotFoundError:
-        day_index = {}
+    lock = day_indexs_locks.get(day_string, None)
+    if not lock:
+        lock = Lock()
+        day_indexs_locks[day_string] = lock
 
-    minute_key = repr(minute.timestamp())
-    mined_that_minute = day_index.get(minute_key, [])
-    mined_that_minute.append(hex(block_hash))
-    day_index[minute_key] = mined_that_minute
+    with lock:
+        try:
+            with open(day_index_file_path, "r") as index_file:
+                day_index = json.load(index_file)
+        except FileNotFoundError:
+            day_index = {}
 
-    with open(day_index_file_path, "w") as index_file:
-        json.dump(day_index, index_file, sort_keys=True, indent=4)
+        minute_key = repr(minute.timestamp())
+        mined_that_minute = day_index.get(minute_key, [])
+        mined_that_minute.append(hex(block_hash))
+        day_index[minute_key] = mined_that_minute
+
+        with open(day_index_file_path, "w") as index_file:
+            json.dump(day_index, index_file, sort_keys=True, indent=4)
 
 def writer_server():
     Path("./blockchain_files/minutes_index").mkdir(parents=True, exist_ok=True)
@@ -47,15 +54,7 @@ def writer_server():
     block_appender_socket = serversocket.accept()
     while True:
         block_hash, block = recv_hash_and_block_json(block_appender_socket)
-
-        print(block_hash)
-        print(block)
-        # i have choosen to use threads because it is allmost all i/o,
-        # so paralelism between instructions is not needed,
-        # and multithreading if ligther than multiprocessing
-        # TODO thread pool
-        writer_t = Thread(target=write_block, args=((block_hash), (block),))
-        writer_t.start()
+        write_block(block_hash, block)
 
 def read_block(client_socket):
     block_hash = recv_hash(client_socket)
@@ -77,6 +76,9 @@ def main():
     writers_t = Thread(target=writer_server)
     writers_t.start()
 
+    # i have choosen to use threads because it is allmost all i/o,
+    # so paralelism between instructions is not needed,
+    # and multithreading if ligther than multiprocessing
     read_thread_pool = ThreadPoolExecutor(READ_THREADS_AMOUNT)
 
     server_socket = SafeTCPSocket.newServer(STORAGE_MANAGER_READ_PORT)
