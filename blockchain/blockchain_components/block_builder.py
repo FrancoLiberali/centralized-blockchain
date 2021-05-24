fda9b99cba03b97af1dee5fd0392d27efe2ba82b
Franco Liberali <franco.liberali@gmail.com>
2021-05-24 00:28:36 -0300

    construir block cuando el limite de tiempo se alcanza sin necesidad de que llegue otro bloque

--- blockchain/blockchain_components/block_builder.py
+++ blockchain/blockchain_components/block_builder.py
@@ -1,23 +1,27 @@
 import datetime
+import threading
+import time
 from common.common import Block, \
     BLOCK_BUILDER_PORT, \
     CHUNK_SIZE_LEN_IN_BYTES,\
     MAX_ENTRIES_AMOUNT, \
-    TARGET_TIME_IN_SECONDS, \
     MAX_ENTRY_SIZE_IN_BYTES
 from common.safe_tcp_socket import SafeTCPSocket
 from common.responses import respond_bad_request, respond_ok, respond_service_unavaliable
 from common.logger import Logger
 
+BLOCK_BUILD_LIMIT_IN_SECONDS = 12
+
+logger = Logger("Block builder")
+lock = threading.Lock()
+chunks = []
+start_time = datetime.datetime.now()
 
 def main(miners_coordinator_queue):
-    logger = Logger("Block builder")
     serversocket = SafeTCPSocket.newServer(BLOCK_BUILDER_PORT)
-    chunks = []
-    start_time = None
+    lauch_thread_to_verify_elapsed_time(miners_coordinator_queue)
 
     while True:
-        # TODO DUDA meter el client adress en algun lado para poder saber quien lo mando? Podria pero no es obligatorio
         clientsocket, client_adress = serversocket.accept()
         if not miners_coordinator_queue.full():
             chunk_size = clientsocket.recv_int(CHUNK_SIZE_LEN_IN_BYTES)
@@ -27,24 +31,44 @@ def main(miners_coordinator_queue):
             chunk = clientsocket.recv_string(chunk_size)
             logger.info(f"Received chuck from client {client_adress}: {chunk}")
 
-            # the start_time if from the acceptation of the first chunk to no to take into account
-            # the time that the miners_coordinator_queue is full
-            if len(chunks) == 0:
-                start_time = datetime.datetime.now()
-            chunks.append(chunk)
-
-            now = datetime.datetime.now()
-            # TODO DUDA problema: es necesario que llegue un nuevo chunck para esto, quizas hay que chequearlo de otra forma. Si, que se mine solo cuando pasa el tiempo
-            elapsed_time = (now - start_time).total_seconds()
-
-            if len(chunks) == MAX_ENTRIES_AMOUNT or elapsed_time >= TARGET_TIME_IN_SECONDS:
-                block = Block(chunks)
-                logger.info(
-                    f"Block appended to Miner's coordinator queue: {block}"
-                )
-                miners_coordinator_queue.put(block)
-                chunks = []
+            with lock:
+                chunks.append(chunk)
+                if len(chunks) == MAX_ENTRIES_AMOUNT or get_elapsed_time_is_bigger_than_limit():
+                    send_chunks_to_be_mined(miners_coordinator_queue)
 
             respond_ok(clientsocket)
         else:
             respond_service_unavaliable(clientsocket)
+
+def get_elapsed_time_is_bigger_than_limit():
+    now = datetime.datetime.now()
+    elapsed_time = (now - start_time).total_seconds()
+    return elapsed_time >= BLOCK_BUILD_LIMIT_IN_SECONDS
+
+def lauch_thread_to_verify_elapsed_time(miners_coordinator_queue):
+    elapsed_time_verifier = threading.Thread(
+        target=verify_elapsed_time, args=((miners_coordinator_queue),))
+    elapsed_time_verifier.start()
+
+def verify_elapsed_time(miners_coordinator_queue):
+    time.sleep(BLOCK_BUILD_LIMIT_IN_SECONDS)
+    with lock:
+        # not get_elapsed_time_is_bigger_than_limit means that MAX_ENTRIES_AMOUNT has been reached
+        # while sleep and this verifier is deprecated
+        if get_elapsed_time_is_bigger_than_limit() and len(chunks) > 0:
+            logger.info(
+                f"Time limit to build block passed, building block"
+            )
+            send_chunks_to_be_mined(miners_coordinator_queue)
+
+def send_chunks_to_be_mined(miners_coordinator_queue):
+    global chunks
+    global start_time
+    block = Block(chunks)
+    logger.info(
+        f"Block appended to Miner's coordinator queue: {block}"
+    )
+    miners_coordinator_queue.put(block)
+    chunks = []
+    start_time = datetime.datetime.now()
+    lauch_thread_to_verify_elapsed_time(miners_coordinator_queue)
