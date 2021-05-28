09fd953609f5af4ae41672f9a5606ff4876f1df1
Franco Liberali <franco.liberali@gmail.com>
2021-05-27 21:15:36 -0300

    fix nombres 2

--- storage_manager/main.py
+++ storage_manager/main.py
@@ -7,7 +7,7 @@ from components.block_writer import writer_server
 from components.common import MINUTES_INDEX_PATH
 from config.envvars import STORAGE_MANAGER_MINED_PER_MINUTE_PORT_KEY, STORAGE_MANAGER_MINED_PER_MINUTE_PROCESS_AMOUNT_KEY, STORAGE_MANAGER_READ_PORT_KEY, STORAGE_MANAGER_READ_PROCESS_AMOUNT_KEY, STORAGE_MANAGER_WRITE_PORT_KEY, get_config_params
 from logger.logger import configure_log
-from components.mined_per_minute import mined_per_minute_server
+from components.mined_per_minute_getter import mined_per_minute_server
 
 def main():
     configure_log()
